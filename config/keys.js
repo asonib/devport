b642a6f3d6e356b3ec47d7eee2d1d29de79cfb94
Aryan Soni <aryan.soni@btech.christuniversity.in>
2020-05-20 01:56:29 +0530

    fixes

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-exports.KeysAccess = {
-    mongoURI: 'mongodb+srv://asonib:ASBdev75@classifier-htisx.mongodb.net/reactapp?retryWrites=true&w=majority',
-    jwtSecret : 'asb75E',
-    gitHubClientID : '5771d31e1ff3d98dfa73',
-    gitHubSecretKey : '4fd2e7a8851535db815a3f00257fbe6cd1f856c4'
-}
