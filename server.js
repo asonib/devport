33835927886e433e55ef9f9c917a04ace47a4282
Aryan Soni <aryan.soni@btech.christuniversity.in>
2020-05-22 20:04:09 +0530

    client -auth routes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.get('/', (req, res) => {
-    res.send('Hello, Welcome To React Server');
+    res.send('Hello, Welcome To React App Backend Server');
 });
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
