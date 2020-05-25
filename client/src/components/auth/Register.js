e73a6cebe5932cbe29ad2fd942b8f18db4505305
Aryan Soni <aryan.soni@btech.christuniversity.in>
2020-05-26 00:08:10 +0530

    added statehook -register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,41 +1,64 @@
 import React from 'react'
-import {Fragment} from 'react';
+import { Fragment, useState } from 'react';
 export const Register = () => {
-    return <Fragment>
-        <h1 class="large text-primary">Sign Up</h1>
-      <p class="lead"><i class="fas fa-user"></i> Create Your Account</p>
-      <form class="form" action="create-profile.html">
-        <div class="form-group">
-          <input type="text" placeholder="Name" name="name" required />
-        </div>
-        <div class="form-group">
-          <input type="email" placeholder="Email Address" name="email" />
-          <small class="form-text"
-            >This site uses Gravatar so if you want a profile image, use a
+
+  const [formData, setformData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+  const { name, email, password, password2 } = formData;
+  const onChange = e => setformData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e =>{
+    e.preventDefault();
+    if(password2 !== password){
+      console.log('Password Do Not Match');
+    }else{
+      console.log(formData);
+    }
+  }
+
+  return <Fragment>
+    <h1 className="large text-primary">Sign Up</h1>
+    <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+    <form className="form" onSubmit={e => onSubmit(e)}>
+      <div className="form-group">
+        <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+      </div>
+      <div className="form-group">
+        <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
+        <small className="form-text"
+        >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
-          >
-        </div>
-        <div class="form-group">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            minLength="6"
-          />
-        </div>
-        <div class="form-group">
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="password2"
-            minLength="6"
-          />
-        </div>
-        <input type="submit" class="btn btn-primary" value="Register" />
-      </form>
-      <p class="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
-      </p>
-    </Fragment>
+        >
+      </div>
+      <div className="form-group">
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={e => onChange(e)}
+          minLength="6"
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          name="password2"
+          value={password2}
+          onChange={e => onChange(e)}
+          minLength="6"
+        />
+      </div>
+      <input type="submit" className="btn btn-primary" value="Register" />
+    </form>
+    <p className="my-1">
+      Already have an account? <a href="login.html">Sign In</a>
+    </p>
+  </Fragment>
 }
 export default Register;
