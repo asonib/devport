4d195a8ced9fd105c5e4d9887ffe7a6e92c6a3aa
Aryan Soni <aryan.soni@btech.christuniversity.in>
2020-06-10 22:24:41 +0530

    profile current user

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
